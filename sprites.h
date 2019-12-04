dff5104fac6ca342668ca5af77bfd6e267a3ac90
Julien <contact.orlok@gmail.com>
2019-12-04 14:26:46 +0100

    [Julien] : Adding all doors.

--- sprites.h
+++ sprites.h
@@ -6,7 +6,7 @@
 /*   By: pasosa-s <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/29 16:43:56 by pasosa-s          #+#    #+#             */
-/*   Updated: 2019/12/04 11:47:36 by jmousset         ###   ########.fr       */
+/*   Updated: 2019/12/04 14:26:25 by jmousset         ###   ########.fr       */
 /*   Updated: 2019/12/02 18:34:03 by pasosa-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
@@ -39,10 +39,10 @@
 # define PRICE_TAG_5 "textures/typo_price_tag_5.XPM"
 
 # define DOOR_CLOSED "textures/texture_door_closed.XPM"
+# define DOOR_CURSE "textures/texture_door_curse.XPM"
+# define DOOR_GOLDEN_CLOSED "textures/texture_door_golden_closed.XPM"
+# define DOOR_GOLDEN_OPEN "textures/texture_door_golden_open.XPM"
 # define DOOR_OPEN "textures/texture_door_open.XPM"
-# define DOOR_GOLDEN_CLOSED "textures/texture_door_closed.XPM"
-# define DOOR_GOLDEN_OPEN "textures/texture_door_open.XPM"
-# define DOOR_SACRIFICE "textures/texture_door_open.XPM"
 # define FLOOR "textures/texture_floor.XPM"
 # define FLOOR2 "textures/texture_floor2.XPM"
 # define FLOOR3 "textures/texture_floor3.XPM"
