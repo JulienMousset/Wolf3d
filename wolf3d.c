260048acd318af88a9df54f60e343cb71662f7d7
Julien <contact.orlok@gmail.com>
2019-12-06 15:57:21 +0100

    [Julien] : Changing player's position and speed AND lvl1 almost done.

--- wolf3d.c
+++ wolf3d.c
@@ -6,7 +6,7 @@
 /*   By: pasosa-s <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/28 13:57:07 by pasosa-s          #+#    #+#             */
-/*   Updated: 2019/12/04 17:19:54 by pasosa-s         ###   ########.fr       */
+/*   Updated: 2019/12/06 14:50:37 by jmousset         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,7 +47,7 @@ void	set_values(t_map *map)
 	map->bool_print_price = 0;
 	map->pick_coin = 0;
 	map->container = 3;
-	map->pick_heart = 1;
+	map->pick_heart = 6;
 	map->pick_key = 0;
 	map->item_map = 1;
 	map->item_heels = 1;
--- wolf3d.h
+++ wolf3d.h
@@ -6,7 +6,7 @@
 /*   By: jmousset <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/31 12:23:00 by jmousset          #+#    #+#             */
-/*   Updated: 2019/12/04 21:21:13 by pasosa-s         ###   ########.fr       */
+/*   Updated: 2019/12/06 14:05:47 by jmousset         ###   ########.fr       */
 /*   Updated: 2019/11/27 19:34:28 by jmousset         ###   ########.fr       */
 /*   Updated: 2019/11/27 12:47:03 by pasosa-s         ###   ########.fr       */
 /*   Updated: 2019/11/15 10:03:20 by jmousset         ###   ########.fr       */
@@ -34,7 +34,7 @@
 # define TS 64
 # define THREADS 4
 
-# define MOVE_SPEED 0.15
+# define MOVE_SPEED 0.125
 # define ROT_SPEED 0.08
 # define SCALE_MS 5
 
